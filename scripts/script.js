e07a57ea6a896e2628fc1506256f61075a0f6e4e
saransrini03 <saransrini003@gmail.com>
2025-04-30 12:35:51 +0530

    added nextjs

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-fetch("templates/index.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("app").innerHTML = data;
-  })
-  .catch((error) => {
-    console.error("Error loading template:", error);
-  });
-
